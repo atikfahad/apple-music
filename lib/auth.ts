75d2617e26a828a648ae9026215f8525909ef893
Atik Fahad <fahadatik@gmail.com>
2021-12-30 08:04:25 +0600

    developed auth library for validation

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken'
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from './prisma'
+
+export const validateRoute = (handler) => {
+    return async (req: NextApiRequest, res: NextApiResponse) => {
+        const { TRAX_ACCESS_TOKEN: token } = req.cookies; // destructured then renamed it!
+        // const token = req.cookies.TRAX_ACCESS_TOKEN same thing, previous is if you have multiple variables
+        if (token) {
+            let user;
+            try {
+                const { id } = jwt.verify(token, 'hello');
+                user = await prisma.user.findUnique({
+                    where: { id }
+                })
+                if (!user) {
+                    throw new Error('Not real user')
+                }
+            }
+            catch (error) {
+                res.status(401)
+                res.json({ error: 'Not authorized' })
+                return
+            }
+
+            return handler(req, res, user)
+        }
+        res.status(401)
+        res.json({ error: 'Not authorized' })
+    }
+}
