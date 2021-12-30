f479a0179050e688439a7591778d3f806a1980fa
Atik Fahad <fahadatik@gmail.com>
2021-12-30 21:30:04 +0600

    created custom hooks

---
+++ lib/hooks.ts
@@ -0,0 +1,21 @@
+import useSWR from "swr";
+import fetcher from "./fetcher";
+
+export const useMe = () => {
+    const { data, error } = useSWR('/me', fetcher)
+    return {
+        user: data,
+        isLoading: !data && !error,
+        isError: error,
+    }
+}
+// basically whats happening here is directly calling api we are using custom hooks where we have used some cacheing strategy
+
+export const usePlaylist = () => {
+    const { data, error } = useSWR('/playlist', fetcher)
+    return {
+        playlists: data || [],
+        isLoading: !data && !error,
+        isError: error
+    }
+}
