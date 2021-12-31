b74b937f60d640391f8bae5280bd06c28ea3c3bd
Atik Fahad <fahadatik@gmail.com>
2021-12-31 22:04:46 +0600

    home page initial section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import GradientLayout from '../components/gradientLayout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>home</div>
+    <GradientLayout
+      roundImage="true"
+      color="blue" subtitle="profile" title="Atik Fahad" description="15 public playlists"
+      image="profile.png">
+      <div>Home</div>
+    </GradientLayout>
   )
 }
