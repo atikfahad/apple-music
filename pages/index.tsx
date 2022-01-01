12e09a6560861b91f8dd9ac481e19d5e8f46451c
Atik Fahad <fahadatik@gmail.com>
2022-01-01 14:04:01 +0600

    top artist module design in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,48 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Box, Flex, Text } from '@chakra-ui/layout'
 import GradientLayout from '../components/gradientLayout'
-import styles from '../styles/Home.module.css'
+import prisma from '../lib/prisma'
+import { Image } from '@chakra-ui/react'
+const Home = ({ artists }) => {
 
-export default function Home() {
   return (
     <GradientLayout
       roundImage="true"
-      color="blue" subtitle="profile" title="Atik Fahad" description="15 public playlists"
+      color="purple" subtitle="profile" title="Atik Fahad" description="15 public playlists"
       image="profile.png">
-      <div>Home</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">Top artist this month</Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="15%">
+              <Box bg="grey.900" borderRadius="4px" padding="15px" width="100%">
+                <Image src="profile.png" borderRadius="100%"></Image>
+                <Box marginTop="20px">
+                  <Text fontSize="large">
+                    {artist.name}
+                  </Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   )
 }
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({})
+  console.log(artists)
+  return {
+    props: { artists }
+  }
+}
+
+
+
+export default Home;
+
