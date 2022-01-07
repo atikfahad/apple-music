514d835c025d567ea3a3a241725dc93690da48c9
Atik Fahad <fahadatik@gmail.com>
2022-01-07 10:35:49 +0600

    id page development

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,7 +1,31 @@
 import { validateToken } from '../../lib/auth'
 import prisma from '../../lib/prisma'
+import GradientLayout from '../../components/gradientLayout'
+const getBGColor = id => {
+    const colors = [
+        'red',
+        'green',
+        'blue',
+        'orange',
+        'purple',
+        'gray',
+        'teal',
+        'yellow'
+    ]
+    return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)]
+}
 const Playlist = ({ playlist }) => {
-    return <div>{playlist.name}</div>
+    const color = getBGColor(playlist.id)
+    return <GradientLayout
+        color={color}
+        roundImage={false}
+        title={playlist.name}
+        subtitle="playlist"
+        description={`${playlist.songs.length} songs`}
+        image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+        <div>tefs</div>
+    </GradientLayout>
 }
 
 export const getServerSideProps = async ({ query, req }) => {
